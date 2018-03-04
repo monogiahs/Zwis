6cc9d813669f044666cb1538e18d8530f520d2d8
monogiahs <zwisss@hotmail.com>
2018-03-04 18:29:04 +0200

    added \n character at the end of each line added some code for CLI

--- main.c
+++ main.c
@@ -5,9 +5,22 @@
 #include "doc_utils.h"
 #include "map.h"
 
+#define MAX_ARG_STR 256
+
 
 int main(int argc, char **argv)
 {
+//        char *str=malloc(MAX_ARG_STR*sizeof(char));
+//        char *temp_str=malloc(MAX_ARG_STR*sizeof(char));
+//        char *tokenSpace;
+//        char *args;
+
+//        int CLIcount;
+
+//        char cchar;
+
+//        CLIcount = 0;
+
         int arg_val;
 
         arg_val = parse_cmd_line_args(argc, argv);
@@ -24,6 +37,38 @@ int main(int argc, char **argv)
 
         load_map_list(argv[get_arg_doc()]);
         print_map_list();
+
+//        /***************CLI***************/
+//        while(1)
+//        {
+//            if(CLIcount>=1)
+//            {
+//                printf("Do you want to continue;(y/n)");
+//                scanf("%c", &cchar);
+//                if(cchar=='y')                      //To continue running commands
+//                {                                   //Reinitialize  below variables
+//                    printf("\n");
+//                    getchar();
+//                }
+//                else if(cchar=='n')                 //To exit CLI
+//                {
+//                    puts("Exiting");
+//                    exit(0);
+//                }
+//            }
+
+//            printf(">>>");
+
+//            //Read a line from stdin and store it to str
+//            if(fgets (str,MAX_ARG_STR,stdin)!=NULL)
+//                strcpy(temp_str,str);
+
+//            tokenSpace = strtok_r(str, " ", &args);
+//            while(tokenSpace != NULL)
+//            {
+//                tokenSpace = strtok_r(NULL," ",&args);
+//            }
+//        }
         //load_inverted_index();
 
         free_map_list();
